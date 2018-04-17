3e90ef6b0a1470008cd56f74101c10e3cb192c78
Vladimir Koshelev <Vladimir.Koshelev@jetbrains.com>
2018-04-17 19:19:08 -0400

    add PyConstantExpression.kt

---
+++ src/main/kotlin/com/jetbrains/python/inspection/PyConstantExpression.kt
@@ -0,0 +1,39 @@
+package com.jetbrains.python.inspection
+
+/*
+import com.intellij.codeInspection.LocalInspectionToolSession
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.PsiElementVisitor
+import com.jetbrains.python.inspections.PyInspection
+import com.jetbrains.python.inspections.PyInspectionVisitor
+import com.jetbrains.python.psi.PyBoolLiteralExpression
+import com.jetbrains.python.psi.PyIfPart
+import com.jetbrains.python.psi.PyIfStatement
+
+
+class PyConstantExpression : PyInspection() {
+
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean,
+                              session: LocalInspectionToolSession): PsiElementVisitor {
+        return Visitor(holder, session)
+    }
+
+    class Visitor(holder: ProblemsHolder?, session: LocalInspectionToolSession) : PyInspectionVisitor(holder, session) {
+
+        override fun visitPyIfStatement(node: PyIfStatement) {
+            super.visitPyIfStatement(node)
+            processIfPart(node.ifPart)
+            for (part in node.elifParts) {
+                processIfPart(part)
+            }
+        }
+
+        private fun processIfPart(pyIfPart: PyIfPart) {
+            val condition = pyIfPart.condition
+            if (condition is PyBoolLiteralExpression) {
+                registerProblem(condition, "The condition is always " + condition.value)
+            }
+        }
+    }
+}
+*/
