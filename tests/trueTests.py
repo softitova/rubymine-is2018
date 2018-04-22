a0e066c7b8aa6b2610b85ccf049a02c6eccb6717
sofatitovass <sof_titova@rambler.ru>
2018-04-22 19:02:40 +0300

    Some tests added

---
+++ tests/trueTests.py
@@ -0,0 +1,29 @@
+if True:
+    pass
+
+if 1 == 1:
+    pass
+
+if 1 - 1 == 1 + 1 - 2:
+    pass
+
+if 1 - 1 == 1 + 1 + 1 - 2 + 4 * 2 - 3 * 3:
+    pass
+
+if 1 < 2:
+    pass
+
+if 2 * 3 < 4 / 2  + 5:
+    pass
+
+if 1 - 1 + 1 == 2 - 2 + 1 and False or True:
+    pass
+
+if not False:
+    pass
+
+if True and True:
+    pass
+
+if True or False:
+    pass
---
+++ tests/undefinedTests.py
@@ -0,0 +1,4 @@
+a = 0
+b = None
+if a + 1 == b:
+    pass
