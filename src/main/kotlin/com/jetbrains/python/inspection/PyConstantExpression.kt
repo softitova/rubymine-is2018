44de8372a696ac9181a591508ee3d2e3ca1e0dbb
sofatitovass <sof_titova@rambler.ru>
2018-04-24 21:30:13 +0300

    Version of task 3 (arithmetical expressions) added null safe version with following operations : +, -, *,

--- src/main/kotlin/com/jetbrains/python/inspection/PyConstantExpression.kt
+++ src/main/kotlin/com/jetbrains/python/inspection/PyConstantExpression.kt
@@ -7,7 +7,6 @@ import com.intellij.psi.PsiElementVisitor
 import com.jetbrains.python.inspections.PyInspection
 import com.jetbrains.python.inspections.PyInspectionVisitor
 import com.jetbrains.python.psi.*
-import com.jetbrains.python.psi.impl.PyBinaryExpressionImpl
 
 
 class PyConstantExpression : PyInspection() {
@@ -27,31 +26,54 @@ class PyConstantExpression : PyInspection() {
             }
         }
 
+        private fun calculate(expr: PyExpression): Long? {
+            fun get(ex: PyExpression) =
+                    when (ex) {
+                        is PyNumericLiteralExpression -> ex.longValue
+                        is PyBinaryExpression -> calculate(ex)
+                        else -> null
+                    }
+
+            return if (expr is PyBinaryExpression) {
+                get(expr.leftExpression)?.let { l ->
+                    get(expr.rightExpression!!)?.let { r ->
+                        when (expr.operator?.specialMethodName) {
+                            "__add__" -> (l + r)
+                            "__sub__" -> (l - r)
+                            "__mul__" -> (l * r)
+                            else -> (l / r)
+                        }
+                    }
+                }
+            } else get(expr)
+
+        }
+
         private fun processIfPart(pyIfPart: PyIfPart) {
             val condition = pyIfPart.condition
+
             fun registerProblem(value: Boolean) {
                 registerProblem(condition, "The condition is always " + value)
             }
+
             if (condition is PyBoolLiteralExpression) {
                 registerProblem(condition.value)
             } else if (condition is PyBinaryExpression) {
-                val lExpr = condition.leftExpression
-                val rExpr = condition.rightExpression
-                if (lExpr is PyNumericLiteralExpression) {
-                    lExpr.longValue?.let { l ->
-                        if (rExpr is PyNumericLiteralExpression)
-                            rExpr.longValue?.let { r ->
-                                when (condition.operator?.specialMethodName) {
-                                    "__eq__" -> registerProblem(l == r)
-                                    "__ne__" -> registerProblem(l != r)
-                                    "__lt__" -> registerProblem(l < r)
-                                    "__le__" -> registerProblem(l <= r)
-                                    "__gt__" -> registerProblem(l > r)
-                                    "__ge__" -> registerProblem(l >= r)
-                                }
+                calculate(condition.leftExpression)?.let { l ->
+                    condition.rightExpression?.let {
+                        calculate(it)?.let { r ->
+                            when (condition.operator?.specialMethodName) {
+                                "__eq__" -> registerProblem(l == r)
+                                "__ne__" -> registerProblem(l != r)
+                                "__lt__" -> registerProblem(l < r)
+                                "__le__" -> registerProblem(l <= r)
+                                "__gt__" -> registerProblem(l > r)
+                                "__ge__" -> registerProblem(l >= r)
                             }
+                        }
                     }
                 }
+
             }
 //            condition?.let {
 //                ExpressionWalker.walk(it.text)?.let {
